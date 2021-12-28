03868d17f7d56865a0eddbf65cb63d1db2f24386
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-12-28 17:37:21 +0530

    Initial Commit

---
+++ Spotify-Clone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spotify-Clone
+//
+//  Created by Manav Deep Singh Lamba on 28/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Spotify-Clone/Spotify_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_CloneApp.swift
+//  Spotify-Clone
+//
+//  Created by Manav Deep Singh Lamba on 28/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
